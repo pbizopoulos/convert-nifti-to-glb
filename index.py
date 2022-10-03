d3a1444bd348d3a90610ee356fe9c85bc0b2f917
your <you>
2022-10-03 16:16:45 +0300

    fix color

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 from js import Blob, document, Uint8Array, window
 from pyodide import create_proxy, to_js
-from scipy.ndimage import zoom
 from skimage.measure import marching_cubes
 import asyncio
 import nibabel as nib
@@ -13,14 +12,13 @@ async def process_file(event):
     for f in fileList:
         data = Uint8Array.new(await f.arrayBuffer())
         nifti_object = nib.Nifti1Image.from_bytes(bytearray(data))
-        nifti_array = nifti_object.get_fdata()
-        after = zoom(nifti_array, (0.5, 0.5, 0.5))
-        (verts, faces, *_) = marching_cubes(nifti_array, 0.5, spacing=(1.0, 1.0, 10.0), step_size=1)
+        (verts, faces, *_) = marching_cubes(nifti_object.get_fdata(), 0.5, spacing=(1.0, 1.0, 10.0), step_size=1)
         tm = trimesh.Trimesh(vertices=verts, faces=faces)
         tm.apply_translation(-tm.center_mass)
         trimesh.repair.fix_inversion(tm)
         trimesh.repair.fill_holes(tm)
         trimesh.smoothing.filter_laplacian(tm, iterations=1)
+        tm.visual.face_colors = [31,119,180,127]
         output = trimesh.exchange.export.export_mesh(tm, 'output.glb')
         content = to_js(output)
         a = document.createElement('a')
