9d5e329d2e49a378c082733e83c5d091fc4cea04
your <you>
2022-10-17 02:21:09 +0300

    fix

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from js import Blob, document, Uint8Array, window
 from nibabel import Nifti1Image
 from skimage.measure import marching_cubes
 import asyncio
+import numpy as np
 import pyodide
 import time
 import trimesh
@@ -17,15 +18,43 @@ async def process_file(event):
     for f in fileList:
         data = Uint8Array.new(await f.arrayBuffer())
         nifti_object = Nifti1Image.from_bytes(bytearray(data))
-        (verts, faces, *_) = marching_cubes(nifti_object.get_fdata(), 0.5, spacing=nifti_object.header.get_zooms(), step_size=1)
-        tm = trimesh.Trimesh(vertices=verts, faces=faces)
-        tm.apply_scale(10**(-3))
-        trimesh.repair.fix_inversion(tm)
-        trimesh.repair.fill_holes(tm)
-        trimesh.smoothing.filter_laplacian(tm, iterations=10)
-        tm.visual = trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial())
-        tm.visual.material.alphaMode = 'BLEND'
-        tm.visual.material.baseColorFactor = [31, 119, 180, 127]
+        volume = nifti_object.get_fdata()
+        volume = volume.astype('float32')
+        if np.unique(volume).size == 2:
+            (verts, faces, *_) = marching_cubes(volume, 0.5, spacing=nifti_object.header.get_zooms(), step_size=1)
+            tm = trimesh.Trimesh(vertices=verts, faces=faces)
+            tm.apply_scale(10**(-3))
+            trimesh.repair.fix_inversion(tm)
+            trimesh.repair.fill_holes(tm)
+            trimesh.smoothing.filter_laplacian(tm, iterations=10)
+            tm.visual = trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial())
+            tm.visual.material.alphaMode = 'BLEND'
+            tm.visual.material.baseColorFactor = [31, 119, 180, 127]
+        elif np.unique(volume).size == 3:
+            volume[volume == 2] = 0
+            (verts, faces, *_) = marching_cubes(volume, spacing=nifti_object.header.get_zooms(), step_size=1)
+            tm = trimesh.Trimesh(vertices=verts, faces=faces)
+            tm.apply_scale(10**(-3))
+            trimesh.repair.fix_inversion(tm)
+            trimesh.repair.fill_holes(tm)
+            # trimesh.smoothing.filter_laplacian(tm, iterations=10)
+            tm.visual = trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial())
+            tm.visual.material.alphaMode = 'BLEND'
+            tm.visual.material.baseColorFactor = [31, 119, 180, 127]
+            volume = nifti_object.get_fdata()
+            volume = volume.astype('float32')
+            volume[volume == 1] = 0
+            volume[volume == 2] = 1
+            (verts2, faces2, *_) = marching_cubes(volume, spacing=nifti_object.header.get_zooms(), step_size=1)
+            tm2 = trimesh.Trimesh(vertices=verts2, faces=faces2)
+            tm2.apply_scale(10**(-3))
+            trimesh.repair.fix_inversion(tm2)
+            trimesh.repair.fill_holes(tm2)
+            # trimesh.smoothing.filter_laplacian(tm2, iterations=10)
+            tm2.visual = trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial())
+            tm2.visual.material.alphaMode = 'BLEND'
+            tm2.visual.material.baseColorFactor = [255, 0, 0, 127]
+            tm = trimesh.util.concatenate([tm, tm2])
         output = trimesh.exchange.export.export_mesh(tm, 'glb')
         content = pyodide.ffi.to_js(output)
         a = document.createElement('a')
