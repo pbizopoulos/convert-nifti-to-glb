9b970ca21ed655c566956e84eb990a5f427fe3e0
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-10-23 14:43:17 +0300

    fix

--- index.py
+++ index.py
@@ -6,14 +6,22 @@ import time
 
 async def process_file(event):
     start_time = time.time()
+    convertButton = document.getElementById('convertButton')
+    convertButton.disabled = True
+    iterationsInputRange = document.getElementById('iterationsInputRange')
+    iterationsInputRange.disabled = True
     loadNiftiFileInputFile = document.getElementById('loadNiftiFileInputFile')
     loadNiftiFileInputFile.disabled = True
+    stepSizeInputRange = document.getElementById('stepSizeInputRange')
+    stepSizeInputRange.disabled = True
     processing_div = document.getElementById('processingDiv')
     processing_div.textContent = 'Converting NifTi to GLB. It might take a few minutes...'
-    fileList = event.target.files.to_py()
+    step_size = int(stepSizeInputRange.value)
+    iterations = int(iterationsInputRange.value)
+    fileList = loadNiftiFileInputFile.files.to_py()
     for f in fileList:
         data = Uint8Array.new(await f.arrayBuffer())
-        output = generate_mesh(data)
+        output = generate_mesh(data, step_size, iterations)
         content = pyodide.ffi.to_js(output)
         a = document.createElement('a')
         document.body.appendChild(a)
@@ -24,9 +32,11 @@ async def process_file(event):
         a.download = 'output.glb'
         a.click()
         window.URL.revokeObjectURL(url)
+        convertButton.disabled = False
         loadNiftiFileInputFile.disabled = False
-        processing_div.textContent = f'Convertion done in {(time.time() - start_time):.1f} seconds.'
+        stepSizeInputRange.disabled = False
+        iterationsInputRange.disabled = False
+        processing_div.textContent = f'Conversion done in {(time.time() - start_time):.1f} seconds.'
 
-
-loadNiftiFileInputFile = document.getElementById('loadNiftiFileInputFile')
-loadNiftiFileInputFile.addEventListener('change', pyodide.ffi.create_proxy(process_file), False)
+convertButton = document.getElementById('convertButton')
+convertButton.addEventListener('click', pyodide.ffi.create_proxy(process_file), False)
--- python/main.py
+++ python/main.py
@@ -6,7 +6,7 @@ import numpy as np
 import trimesh
 
 
-def generate_mesh(data):
+def generate_mesh(data, step_size, iterations):
     if isinstance(data, str):
         if not isfile(data):
             request.urlretrieve(f'https://github.com/pbizopoulos/semi-automatic-annotation-tool/releases/download/dist/{data.split("/")[1]}', data)
@@ -18,28 +18,30 @@ def generate_mesh(data):
     volume = nifti_object.get_fdata()
     volume = volume.astype('float32')
     if np.unique(volume).size == 2:
-        (verts, faces, *_) = marching_cubes(volume, spacing=nifti_object.header.get_zooms(), step_size=1)
+        (verts, faces, *_) = marching_cubes(volume, spacing=nifti_object.header.get_zooms(), step_size=step_size)
         tm = trimesh.base.Trimesh(vertices=verts, faces=faces, visual=trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial(alphaMode='BLEND', baseColorFactor=[31, 119, 180, 127])))
         tm.apply_scale(10 ** (-3))
         trimesh.repair.fix_inversion(tm)
         trimesh.repair.fill_holes(tm)
-        trimesh.smoothing.filter_laplacian(tm, iterations=10)
+        trimesh.smoothing.filter_laplacian(tm, iterations=iterations)
     elif np.unique(volume).size == 3:
         volume[volume == 2] = 0
-        (verts, faces, *_) = marching_cubes(volume, spacing=nifti_object.header.get_zooms(), step_size=1)
+        (verts, faces, *_) = marching_cubes(volume, spacing=nifti_object.header.get_zooms(), step_size=step_size)
         tm = trimesh.base.Trimesh(vertices=verts, faces=faces, visual=trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial(alphaMode='BLEND', baseColorFactor=[31, 119, 180, 127])))
         tm.apply_scale(10 ** (-3))
         trimesh.repair.fix_inversion(tm)
         trimesh.repair.fill_holes(tm)
+        trimesh.smoothing.filter_laplacian(tm, iterations=iterations)
         volume = nifti_object.get_fdata()
         volume = volume.astype('float32')
         volume[volume == 1] = 0
         volume[volume == 2] = 1
-        (verts2, faces2, *_) = marching_cubes(volume, spacing=nifti_object.header.get_zooms(), step_size=1)
+        (verts2, faces2, *_) = marching_cubes(volume, spacing=nifti_object.header.get_zooms(), step_size=step_size)
         tm2 = trimesh.base.Trimesh(vertices=verts2, faces=faces2, visual=trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial(alphaMode='BLEND', baseColorFactor=[255, 0, 0, 127])))
         tm2.apply_scale(10 ** (-3))
         trimesh.repair.fix_inversion(tm2)
         trimesh.repair.fill_holes(tm2)
+        trimesh.smoothing.filter_laplacian(tm2, iterations=iterations)
         tm = trimesh.util.concatenate([tm, tm2])
         tm.visual.material = tm.visual.material.to_pbr()
         tm.visual.material.alphaMode = 'BLEND'
@@ -48,8 +50,8 @@ def generate_mesh(data):
 
 
 def main():
-    generate_mesh(join('bin', 'masks.nii'))
-    generate_mesh(join('bin', 'masks-multiclass.nii'))
+    generate_mesh(join('bin', 'masks.nii'), 2, 1)
+    generate_mesh(join('bin', 'masks-multiclass.nii'), 2, 1)
 
 
 if __name__ == '__main__':
