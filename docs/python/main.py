d1decd9eb268249ced4001536976e040123e69fe
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-03-21 16:11:57 +0200

    .

--- docs/python/main.py
+++ docs/python/main.py
@@ -9,20 +9,20 @@ from skimage.measure import marching_cubes
 
 def generate_mesh(data: str, laplacian_smoothing_iterations: int, marching_cubes_step_size: int) -> bytes:
     if isinstance(data, str):
-        file_name = data.split('/')[-1]
+        file_name = data.split("/")[-1]
         if not isfile(data): # noqa: PTH113
-            request.urlretrieve(f'https://github.com/pbizopoulos/semi-automatic-annotation-tool/releases/download/dist/{file_name}', data)
+            request.urlretrieve(f"https://github.com/pbizopoulos/semi-automatic-annotation-tool/releases/download/dist/{file_name}", data)
         nifti_object = Nifti1Image.from_filename(data)
-        file_name_without_extension = '.'.join(file_name.split('.')[:-1])
-        output_file_name = f'bin/{file_name_without_extension}-step-size-{marching_cubes_step_size}-iterations-{laplacian_smoothing_iterations}.glb'
+        file_name_without_extension = ".".join(file_name.split(".")[:-1])
+        output_file_name = f"bin/{file_name_without_extension}-step-size-{marching_cubes_step_size}-iterations-{laplacian_smoothing_iterations}.glb"
     else:
         nifti_object = Nifti1Image.from_bytes(bytearray(data))
-        output_file_name = 'output.glb'
+        output_file_name = "output.glb"
     volume = nifti_object.get_fdata()
-    volume = volume.astype('float32')
+    volume = volume.astype("float32")
     if np.unique(volume).size == 2: # noqa: PLR2004
         verts, faces, *_ = marching_cubes(volume, spacing=nifti_object.header.get_zooms(), step_size=marching_cubes_step_size)
-        tm = trimesh.base.Trimesh(vertices=verts, faces=faces, visual=trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial(alphaMode='BLEND', baseColorFactor=[31, 119, 180, 127])))
+        tm = trimesh.base.Trimesh(vertices=verts, faces=faces, visual=trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial(alphaMode="BLEND", baseColorFactor=[31, 119, 180, 127])))
         tm.apply_scale(10 ** (-3))
         trimesh.repair.fix_inversion(tm)
         trimesh.repair.fill_holes(tm)
@@ -30,33 +30,33 @@ def generate_mesh(data: str, laplacian_smoothing_iterations: int, marching_cubes
     elif np.unique(volume).size == 3: # noqa: PLR2004
         volume[volume == 2] = 0 # noqa: PLR2004
         verts, faces, *_ = marching_cubes(volume, spacing=nifti_object.header.get_zooms(), step_size=marching_cubes_step_size)
-        tm = trimesh.base.Trimesh(vertices=verts, faces=faces, visual=trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial(alphaMode='BLEND', baseColorFactor=[31, 119, 180, 127])))
+        tm = trimesh.base.Trimesh(vertices=verts, faces=faces, visual=trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial(alphaMode="BLEND", baseColorFactor=[31, 119, 180, 127])))
         tm.apply_scale(10 ** (-3))
         trimesh.repair.fix_inversion(tm)
         trimesh.repair.fill_holes(tm)
         trimesh.smoothing.filter_laplacian(tm, iterations=laplacian_smoothing_iterations)
         volume = nifti_object.get_fdata()
-        volume = volume.astype('float32')
+        volume = volume.astype("float32")
         volume[volume == 1] = 0
         volume[volume == 2] = 1 # noqa: PLR2004
         verts2, faces2, *_ = marching_cubes(volume, spacing=nifti_object.header.get_zooms(), step_size=marching_cubes_step_size)
-        tm2 = trimesh.base.Trimesh(vertices=verts2, faces=faces2, visual=trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial(alphaMode='BLEND', baseColorFactor=[255, 0, 0, 127])))
+        tm2 = trimesh.base.Trimesh(vertices=verts2, faces=faces2, visual=trimesh.visual.TextureVisuals(material=trimesh.visual.material.PBRMaterial(alphaMode="BLEND", baseColorFactor=[255, 0, 0, 127])))
         tm2.apply_scale(10 ** (-3))
         trimesh.repair.fix_inversion(tm2)
         trimesh.repair.fill_holes(tm2)
         trimesh.smoothing.filter_laplacian(tm2, iterations=laplacian_smoothing_iterations)
         tm = trimesh.util.concatenate([tm, tm2])
         tm.visual.material = tm.visual.material.to_pbr()
-        tm.visual.material.alphaMode = 'BLEND'
+        tm.visual.material.alphaMode = "BLEND"
         tm.visual.material.baseColorFactor[-1] = 127
     output: bytes = trimesh.exchange.export.export_mesh(tm, output_file_name)
     return output
 
 
 def main() -> None:
-    generate_mesh('bin/masks.nii', 1, 2)
-    generate_mesh('bin/masks-multiclass.nii', 1, 2)
+    generate_mesh("bin/masks.nii", 1, 2)
+    generate_mesh("bin/masks-multiclass.nii", 1, 2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -5,37 +5,37 @@ from playwright.sync_api import Error, sync_playwright
 
 
 def main() -> None:
-    input_nii_file_path = Path('bin/masks-multiclass.nii')
+    input_nii_file_path = Path("bin/masks-multiclass.nii")
     if not input_nii_file_path.is_file():
         with sync_playwright() as playwright:
             browser = playwright.chromium.launch()
             page = browser.new_page()
-            page.goto('https://github.com/pbizopoulos/semi-automatic-annotation-tool/releases/latest')
-            page.click('text=Assets')
+            page.goto("https://github.com/pbizopoulos/semi-automatic-annotation-tool/releases/latest")
+            page.click("text=Assets")
             with page.expect_download() as download_info:
-                page.click('text=masks-multiclass.nii')
+                page.click("text=masks-multiclass.nii")
             download = download_info.value
             download.save_as(input_nii_file_path)
             browser.close()
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch()
-        context = browser.new_context(record_video_dir='bin/')
+        context = browser.new_context(record_video_dir="bin/")
         page = context.new_page()
         timeout = 200000
         page.set_default_timeout(timeout)
         page.set_default_navigation_timeout(timeout)
-        page.on('pageerror', page_error)
-        page.goto('https://nifti-to-glb-conversion-tool.incisive.iti.gr/')
-        page.set_input_files('#load-nifti-file-input-file', input_nii_file_path.resolve().as_posix())
+        page.on("pageerror", page_error)
+        page.goto("https://nifti-to-glb-conversion-tool.incisive.iti.gr/")
+        page.set_input_files("#load-nifti-file-input-file", input_nii_file_path.resolve().as_posix())
         with page.expect_download() as download_info:
-            page.click('#convert-button')
+            page.click("#convert-button")
         download = download_info.value
-        download.save_as('bin/masks-multiclass-step-size-2-iterations-1.glb')
-        with Path('bin/masks-multiclass-step-size-2-iterations-1.glb').open('rb') as file:
-            assert sha256(file.read()).hexdigest() == 'a0a64cb2193fb0919525c51e6f83fe7811c6bd42f93481c0c6cfce204a81eaad'
-        page.screenshot(path='bin/screenshot.png')
-        with Path('bin/screenshot.png').open('rb') as file:
-            assert sha256(file.read()).hexdigest() == '947b85dcf07c39fd78ad776b165d22e9a9d595191d37e02ae3abfbf09e0f859c'
+        download.save_as("bin/masks-multiclass-step-size-2-iterations-1.glb")
+        with Path("bin/masks-multiclass-step-size-2-iterations-1.glb").open("rb") as file:
+            assert sha256(file.read()).hexdigest() == "a0a64cb2193fb0919525c51e6f83fe7811c6bd42f93481c0c6cfce204a81eaad"
+        page.screenshot(path="bin/screenshot.png")
+        with Path("bin/screenshot.png").open("rb") as file:
+            assert sha256(file.read()).hexdigest() == "947b85dcf07c39fd78ad776b165d22e9a9d595191d37e02ae3abfbf09e0f859c"
         context.close()
         browser.close()
 
@@ -44,5 +44,5 @@ def page_error(exception: Error) -> None:
     raise exception
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
