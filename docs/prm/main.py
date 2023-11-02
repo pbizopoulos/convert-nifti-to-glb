9ad748982f9aba88abb68e101a6e1e9bcd879613
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-11-02 12:03:08 +0200

    .

--- docs/prm/main.py
+++ docs/prm/main.py
@@ -1,9 +1,13 @@
+import sys
 from pathlib import Path
 from urllib import request
 
 import numpy as np
+import pyodide
 import trimesh
-from nibabel import Nifti1Image  # type: ignore[attr-defined]
+from js import Blob, Uint8Array, document, window
+from nibabel import Nifti1Image
+from pyodide.ffi.wrappers import add_event_listener
 from skimage.measure import marching_cubes
 
 
@@ -26,7 +30,7 @@ def generate_mesh(data: str, iterations: int, step_size: int) -> bytes:
     if np.unique(volume).size == 2:  # noqa: PLR2004
         verts, faces, *_ = marching_cubes(
             volume,
-            spacing=nifti_object.header.get_zooms(),  # type: ignore[no-untyped-call]
+            spacing=nifti_object.header.get_zooms(),
             step_size=step_size,
         )
         tm = trimesh.base.Trimesh(
@@ -47,7 +51,7 @@ def generate_mesh(data: str, iterations: int, step_size: int) -> bytes:
         volume[volume == 2] = 0  # noqa: PLR2004
         verts, faces, *_ = marching_cubes(
             volume,
-            spacing=nifti_object.header.get_zooms(),  # type: ignore[no-untyped-call]
+            spacing=nifti_object.header.get_zooms(),
             step_size=step_size,
         )
         tm = trimesh.base.Trimesh(
@@ -70,7 +74,7 @@ def generate_mesh(data: str, iterations: int, step_size: int) -> bytes:
         volume[volume == 2] = 1  # noqa: PLR2004
         verts2, faces2, *_ = marching_cubes(
             volume,
-            spacing=nifti_object.header.get_zooms(),  # type: ignore[no-untyped-call]
+            spacing=nifti_object.header.get_zooms(),
             step_size=step_size,
         )
         tm2 = trimesh.base.Trimesh(
@@ -93,9 +97,67 @@ def generate_mesh(data: str, iterations: int, step_size: int) -> bytes:
     return output
 
 
+async def on_click_convert_button(_: str) -> None:
+    convert_button = document.getElementById("convert-button")
+    convert_button.disabled = True
+    laplacian_smoothing_iterations_input_range = document.getElementById(
+        "laplacian-smoothing-iterations-input-range",
+    )
+    laplacian_smoothing_iterations_input_range.disabled = True
+    load_nifti_file_input_file = document.getElementById("load-nifti-file-input-file")
+    load_nifti_file_input_file.disabled = True
+    marching_cubes_step_size_input_range = document.getElementById(
+        "marching-cubes-step-size-input-range",
+    )
+    marching_cubes_step_size_input_range.disabled = True
+    processing_div = document.getElementById("processing-div")
+    processing_div.textContent = (
+        "Converting NIfTI to GLB. It might take a few minutes..."
+    )
+    marching_cubes_step_size = int(marching_cubes_step_size_input_range.value)
+    laplacian_smoothing_iterations = int(
+        laplacian_smoothing_iterations_input_range.value,
+    )
+    file_list = load_nifti_file_input_file.files.to_py()
+    for file in file_list:
+        data = Uint8Array.new(await file.arrayBuffer())
+        try:
+            output = generate_mesh(
+                data,
+                laplacian_smoothing_iterations,
+                marching_cubes_step_size,
+            )
+        except Exception:  # noqa: BLE001
+            convert_button.disabled = False
+            load_nifti_file_input_file.disabled = False
+            marching_cubes_step_size_input_range.disabled = False
+            laplacian_smoothing_iterations_input_range.disabled = False
+            processing_div.textContent = "Out of memory. Try reducing the iterations and/or increasing the step size"  # noqa: E501
+            sys.exit()
+        content = pyodide.ffi.to_js(output)
+        a = document.createElement("a")
+        document.body.appendChild(a)
+        a.style = "display: none"
+        blob = Blob.new([content])
+        url = window.URL.createObjectURL(blob)
+        a.href = url
+        file_name_without_extension = ".".join(file.name.split(".")[:-1])
+        a.download = f"{file_name_without_extension}-step-size-{marching_cubes_step_size}-iterations-{laplacian_smoothing_iterations}.glb"  # noqa: E501
+        a.click()
+        window.URL.revokeObjectURL(url)
+        convert_button.disabled = False
+        load_nifti_file_input_file.disabled = False
+        marching_cubes_step_size_input_range.disabled = False
+        laplacian_smoothing_iterations_input_range.disabled = False
+        processing_div.textContent = "Conversion done."
+
+
 def main() -> None:
-    generate_mesh("tmp/masks.nii", 1, 2)
-    generate_mesh("tmp/masks-multiclass.nii", 1, 2)
+    add_event_listener(
+        document.getElementById("convert-button"),
+        "click",
+        on_click_convert_button,
+    )
 
 
 if __name__ == "__main__":
